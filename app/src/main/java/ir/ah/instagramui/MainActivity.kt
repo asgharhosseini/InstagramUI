edfb0c06b6cdfebdd0686f889c6a12be0c97f471
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-01 19:38:34 +0430

    Init Commit

---
+++ app/src/main/java/ir/ah/instagramui/MainActivity.kt
@@ -0,0 +1,32 @@
+package ir.ah.instagramui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import ir.ah.instagramui.ui.theme.InstagramUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            InstagramUITheme {
+
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    InstagramUITheme {
+
+    }
+}
